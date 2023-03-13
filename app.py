3a063306d9cf1058006d752327649ab833bea21c
Skyrme <skyrmo@v1370-0a9e0c45.wifi.cf.ac.uk>
2023-03-13 17:12:14 +0000

    where we are at by the end of monday.

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# import os
+import os
 
 
 from flask import render_template
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '<please generate a new secret key>'
+app.config['SECRET_KEY'] = '<please generate a new secret key>'
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-# app.config['SQLALCHEMY_DATABASE_URI'] =\
-#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 @app.route("/")
