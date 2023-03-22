92d0ad9808ac457705f6285d74c52f6d6e3f8c5e
Owain <skyrmeog@cardiff.ac.uk>
2023-03-22 11:21:18 +0800

    added simple login redirect and added user to session.

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
 from . import auth
 
 
-@auth.route('/')
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@auth.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        session['username'] = form.username.data
+        print(form.password.data)
+        session['password'] = form.password.data
+        return redirect(url_for('students.index'))
+    return render_template('auth/login.html', form=form, username=session.get('username'), password=session.get('password'))
 
 
 @auth.route('/register')
