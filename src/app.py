d105a45dcd911893ef66ec438bdc533a8bc67794
Connor Walsh <walshc10@cardiff.ac.uk>
2023-04-06 10:57:24 +0000

    Removed unnecessary code

--- src/app.py
+++ src/app.py
@@ -2,9 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Welcome to Automated Assessment Tool'
-
 if __name__ == '__main__':
     app.run(debug=True)
