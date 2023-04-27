0ca0147bf37f3c4b1cecb4a4709369a26573f4d3
Charles Ikusan <ikusanca@cardiff.ac.uk>
2023-04-27 07:45:51 +0000

    Upload New File

---
+++ src/models/answer.py
@@ -0,0 +1,29 @@
+from src import db
+
+
+class Answer(db.Model):
+    __tablename__ = 'answers'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    is_correct = db.Column(db.Boolean, default=False)
+    correct_answer = db.Column(db.Text)
+    attempt_id = db.Column(db.Integer, db.ForeignKey('attempts.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+
+    def __init__(self, content: str, is_correct: bool, correct_answer: str, attempt_id: int, question_id: int):
+        self.content = content
+        self.question_id = question_id
+        self.attempt_id = attempt_id
+        self.is_correct = is_correct
+        self.correct_answer = correct_answer
+
+    @staticmethod
+    def create(content, is_correct, correct_answer, attempt_id, question_id):
+        new_answer = Answer(
+            content=content, is_correct=is_correct, correct_answer=correct_answer, attempt_id=attempt_id, question_id=question_id)
+        db.session.add(new_answer)
+        db.session.commit()
+        return new_answer
+
+    def __repr__(self):
+        return '<Answer %r>' % self.content
