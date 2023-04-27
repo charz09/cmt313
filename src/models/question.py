98b056bab80289ebda79ef3905f95ee7e7839573
Charles Ikusan <ikusanca@cardiff.ac.uk>
2023-04-27 07:48:24 +0000

    Delete question.py

--- src/models/question.py
+++
@@ -1,8 +0,0 @@
-from src import db
-from src.models.assessment import Assessment
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    assessment_id = db.Column(db.Integer, db.ForeignKey('assessments.id'), nullable=False)
